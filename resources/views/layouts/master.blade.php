73d8a6b280f6e6b0e8382a106fe2f76f3e0aaeb7
Prasun Paul Chowdhury <prasun@novatree.com>
2017-12-15 23:05:21 +0530

    Body content has been placed in home blade.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,34 +2,6 @@
 <html lang="en">
 @include('layouts/partials/head')
 <body>
-<div class="hero-area">
-	<div class="container-fluid">
-		<ul>
-			<li>
-				<a href="#">
-					sign up
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					login
-				</a>
-			</li>
-			<li>
-				<a href="#">
-					dashboard
-				</a>
-			</li>
-		</ul>
-		<div class="col-md-12 text-center intro-div">
-			<p class="intro">
-				Introducing
-			</p>
-			<a href="#">
-				<img src="{{ asset('assets/images/logo-black.png') }}">
-			</a>
-		</div>
-	</div>
-</div>
+@yield('content')
 </body>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,1 +1,32 @@
 @extends('layouts.master')
+@section('content')
+	<div class="hero-area">
+		<div class="container-fluid">
+			<ul>
+				<li>
+					<a href="#">
+						sign up
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						login
+					</a>
+				</li>
+				<li>
+					<a href="#">
+						dashboard
+					</a>
+				</li>
+			</ul>
+			<div class="col-md-12 text-center intro-div">
+				<p class="intro">
+					Introducing
+				</p>
+				<a href="#">
+					<img src="{{ asset('assets/images/logo-black.png') }}">
+				</a>
+			</div>
+		</div>
+	</div>
+@endsection
