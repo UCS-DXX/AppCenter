e9010cc9f747b97c30b310486fd9b35b7cb6fd60
Prasun Paul Chowdhury <prasun@novatree.com>
2017-12-19 16:04:26 +0530

    Apps blade has been changed.

--- resources/views/pages/apps.blade.php
+++ resources/views/pages/apps.blade.php
@@ -1,22 +1,28 @@
-@extends('master')
-@section('custom-includes')
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<meta name="description" content="">
+	<meta property="" content="">
+	<title>UNACORES</title>
+	<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicon-32x32.png') }}">
+	<link rel="stylesheet" type="text/css"
+	      href="https://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,700">
 	<link rel="stylesheet" href="{{ asset('assets/css/apps.css') }}">
-@endsection
-@section('content')
-	<body>
-		<div class="width-100">
-			<div class="apps-container">
-				<div class="flex-container">
+</head>
+<body>
+<div class="width-100">
+	<div class="apps-container">
+		<div class="flex-container">
+			<div>
+				<a href="{{ URL::to('changeapp/transfer_money') }}">
 					<div>
-						<a href="{{ URL::to('changeapp/transfer_money') }}">
-							<div>
-								<img src="{{ asset('assets/images/money-transfer.jpg') }}" height="150px" width="150px" />
-							</div>
-							<div>Transfer Money</div>
-						</a>
+						<img src="{{ asset('assets/images/money-transfer.jpg') }}" height="150px" width="150px"/>
 					</div>
-				</div>
+					<div>Transfer Money</div>
+				</a>
 			</div>
 		</div>
-	</body>
-@endsection
+	</div>
+</div>
+</body>
