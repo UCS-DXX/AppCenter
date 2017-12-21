b1a7188ee6549a1c2b013e64be5ae6a66283c9d6
Prasun Paul Chowdhury <prasun@novatree.com>
2017-12-21 12:50:59 +0530

    Edit customer page design has been fixed.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -47,8 +47,37 @@ class CustomerController extends Controller
 		$customer = array_shift($customer);
 		$data = array();
 		$data['customer']['id'] = $customer['id'];
+		$data['customer']['app_id'] = $customer['app_id'];
 		$data['customer']['name'] = $customer['name'];
+		$data['customer']['low_balance_alert_at'] = $customer['low_balance_alert_at'];
+		$data['customer']['identity_user_id'] = $customer['identity_user_id'];
+		$data['customer']['allow_neft'] = $customer['allow_neft'];
+		$data['customer']['allow_imps'] = $customer['allow_imps'];
+		$data['customer']['allow_rtgs'] = $customer['allow_rtgs'];
+		$data['customer']['string'] = $customer['string'];
+		$data['customer']['enabled'] = $customer['enabled'];
+		$data['customer']['is_retail'] = $customer['is_retail'];
 		$data['customer']['customer_id'] = $customer['customer_id'];
+		$data['customer']['created_by'] = $customer['created_by'];
+		$data['customer']['updated_by'] = $customer['updated_by'];
+		$data['customer']['created_at'] = $customer['created_at'];
+		$data['customer']['updated_at'] = $customer['updated_at'];
+		$data['customer']['lock_version'] = $customer['lock_version'];
+		$data['customer']['approval_status'] = $customer['approval_status'];
+		$data['customer']['last_action'] = $customer['last_action'];
+		$data['customer']['approved_version'] = $customer['approved_version'];
+		$data['customer']['approved_id'] = $customer['approved_id'];
+		$data['customer']['needs_purpose_code'] = $customer['needs_purpose_code'];
+		$data['customer']['reply_with_bene_name'] = $customer['reply_with_bene_name'];
+		$data['customer']['allow_all_accounts'] = $customer['allow_all_accounts'];
+		$data['customer']['notify_app_code'] = $customer['notify_app_code'];
+		$data['customer']['notify_on_status_change'] = $customer['notify_on_status_change'];
+		$data['customer']['is_filetoapi_allowed'] = $customer['is_filetoapi_allowed'];
+		$data['customer']['allow_apbs'] = $customer['allow_apbs'];
+		$data['customer']['apbs_user_no'] = $customer['apbs_user_no'];
+		$data['customer']['notification_sent_at'] = $customer['notification_sent_at'];
+		$data['customer']['allowed_relns'] = $customer['allowed_relns'];
+		$data['customer']['bene_backend'] = $customer['bene_backend'];
 		return view('apps.' . $app . '.edit-customer', array('data' => $data));
 	}
 	
--- resources/views/apps/transfer_money/edit-customer.blade.php
+++ resources/views/apps/transfer_money/edit-customer.blade.php
@@ -13,6 +13,15 @@
 					<div class="row">
 						<div class="col-sm-12">
 							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['app_id'] }}" name="app_id" spellcheck="false"
+								       data-msg-required="Please enter your app ID." required>
+								<label class="md-control-label">App ID</label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-12">
+							<div class="md-form-group md-label-floating">
 								<input class="md-form-control" type="text" value="{{ $data['customer']['name'] }}" name="name" spellcheck="false"
 								       data-msg-required="Please enter your name." required>
 								<label class="md-control-label">Name</label>
@@ -25,6 +34,192 @@
 								<input readonly class="md-form-control" type="text" value="{{ $data['customer']['customer_id'] }}" name="customer_id" spellcheck="false"
 								       data-msg-required="Please enter your customer ID." required>
 								<label class="md-control-label">Customer ID</label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['low_balance_alert_at'] }}" name="low_balance_alert_at" spellcheck="false"
+								       data-msg-required="Please enter low balance alert at." required>
+								<label class="md-control-label">Low Balance Alert At</label>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['identity_user_id'] }}" name="identity_user_id" spellcheck="false"
+								       data-msg-required="Please enter identity user id." required>
+								<label class="md-control-label">Identity User Id</label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="custom-controls-stacked m-t" style="margin-bottom: 20px; padding: 20px 0; position: relative;">
+								<label class="custom-control custom-control-primary custom-checkbox">
+									<input class="custom-control-input" type="checkbox" name="allow_neft" @if(isset($data['customer']['allow_neft']) AND $data['customer']['allow_neft'] != null) checked="checked" @endif>
+									<span class="custom-control-indicator"></span>
+									<span class="custom-control-label" style="font-size: 16px;">Allow NEFT</span>
+								</label>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="custom-controls-stacked m-t" style="margin-bottom: 20px; padding: 20px 0; position: relative;">
+								<label class="custom-control custom-control-primary custom-checkbox">
+									<input class="custom-control-input" type="checkbox" name="allow_imps" @if(isset($data['customer']['allow_imps']) AND $data['customer']['allow_imps'] != null) checked="checked" @endif>
+									<span class="custom-control-indicator"></span>
+									<span class="custom-control-label" style="font-size: 16px;">Allow IMPS</span>
+								</label>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="custom-controls-stacked m-t" style="margin-bottom: 20px; padding: 20px 0; position: relative;">
+								<label class="custom-control custom-control-primary custom-checkbox">
+									<input class="custom-control-input" type="checkbox" name="allow_rtgs" @if(isset($data['customer']['allow_rtgs']) AND $data['customer']['allow_rtgs'] != null) checked="checked" @endif>
+									<span class="custom-control-indicator"></span>
+									<span class="custom-control-label" style="font-size: 16px;">Allow RTGS</span>
+								</label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="custom-controls-stacked m-t" style="margin-bottom: 20px; padding: 20px 0; position: relative;">
+								<label class="custom-control custom-control-primary custom-checkbox">
+									<input class="custom-control-input" type="checkbox" name="string" @if(isset($data['customer']['string']) AND $data['customer']['string'] != null) checked="checked" @endif>
+									<span class="custom-control-indicator"></span>
+									<span class="custom-control-label" style="font-size: 16px;">String</span>
+								</label>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="custom-controls-stacked m-t" style="margin-bottom: 20px; padding: 20px 0; position: relative;">
+								<label class="custom-control custom-control-primary custom-checkbox">
+									<input class="custom-control-input" type="checkbox" name="enabled" @if(isset($data['customer']['enabled']) AND $data['customer']['enabled'] != null) checked="checked" @endif>
+									<span class="custom-control-indicator"></span>
+									<span class="custom-control-label" style="font-size: 16px;">Enabled</span>
+								</label>
+							</div>
+						</div>
+						<div class="col-sm-4">
+							<div class="custom-controls-stacked m-t" style="margin-bottom: 20px; padding: 20px 0; position: relative;">
+								<label class="custom-control custom-control-primary custom-checkbox">
+									<input class="custom-control-input" type="checkbox" name="is_retail" @if(isset($data['customer']['is_retail']) AND $data['customer']['is_retail'] != null) checked="checked" @endif>
+									<span class="custom-control-indicator"></span>
+									<span class="custom-control-label" style="font-size: 16px;">Is Retail</span>
+								</label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['lock_version'] }}" name="lock_version" spellcheck="false"
+								       data-msg-required="Please enter lock version." required>
+								<label class="md-control-label">Lock Version</label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['approved_version'] }}" name="approved_version" spellcheck="false"
+								       data-msg-required="Please enter approved version." required>
+								<label class="md-control-label">Approved Version</label>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['approved_id'] }}" name="approved_id" spellcheck="false"
+								       data-msg-required="Please enter approved ID." required>
+								<label class="md-control-label">Approved ID</label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-5">
+							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['needs_purpose_code'] }}" name="needs_purpose_code" spellcheck="false"
+								       data-msg-required="Please enter purpose code." required>
+								<label class="md-control-label">Needs Purpose Code</label>
+							</div>
+						</div>
+						<div class="col-xs-7">
+							<div class="md-form-group">
+								<select class="md-form-control" name="reply_with_bene_name" data-msg-required="Reply With Bene Name." required>
+									<option value="" disabled="disabled" selected="selected">Reply With Bene Name</option>
+									<option value="Y">Yes</option>
+									<option value="N">No</option>
+								</select>
+								<label class="md-control-label"></label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-7">
+							<div class="md-form-group">
+								<select class="md-form-control" name="allow_all_accounts" data-msg-required="Allow All Accounts." required>
+									<option value="" disabled="disabled" selected="selected">Allow All Accounts</option>
+									<option value="Y">Yes</option>
+									<option value="N">No</option>
+								</select>
+								<label class="md-control-label"></label>
+							</div>
+						</div>
+						<div class="col-sm-5">
+							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['notify_app_code'] }}" name="notify_app_code" spellcheck="false"
+								       data-msg-required="Please enter purpose code." required>
+								<label class="md-control-label">Notify App Code</label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-6">
+							<div class="md-form-group">
+								<select class="md-form-control" name="notify_on_status_change" data-msg-required="Notify On Status Change." required>
+									<option value="" disabled="disabled" selected="selected">Notify On Status Change</option>
+									<option value="Y">Yes</option>
+									<option value="N">No</option>
+								</select>
+								<label class="md-control-label"></label>
+							</div>
+						</div>
+						<div class="col-xs-6">
+							<div class="md-form-group">
+								<select class="md-form-control" name="is_filetoapi_allowed" data-msg-required="Is Filetoapi Allowed." required>
+									<option value="" disabled="disabled" selected="selected">Is Filetoapi Allowed</option>
+									<option value="Y">Yes</option>
+									<option value="N">No</option>
+								</select>
+								<label class="md-control-label"></label>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-4">
+							<div class="custom-controls-stacked m-t" style="margin-bottom: 20px; padding: 20px 0; position: relative;">
+								<label class="custom-control custom-control-primary custom-checkbox">
+									<input class="custom-control-input" type="checkbox" name="allow_apbs" @if(isset($data['customer']['allow_apbs']) AND $data['customer']['allow_apbs'] != null) checked="checked" @endif>
+									<span class="custom-control-indicator"></span>
+									<span class="custom-control-label" style="font-size: 16px;">Allow APBS</span>
+								</label>
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="md-form-group md-label-floating">
+								<input class="md-form-control" type="text" value="{{ $data['customer']['apbs_user_no'] }}" name="apbs_user_no" spellcheck="false"
+								       data-msg-required="Please enter purpose code." required>
+								<label class="md-control-label">APBS User No</label>
+							</div>
+						</div>
+						<div class="col-sm-5">
+							<div class="custom-controls-stacked m-t" style="margin-bottom: 20px; padding: 20px 0; position: relative;">
+								<label class="custom-control custom-control-primary custom-checkbox">
+									<input class="custom-control-input" type="checkbox" name="allowed_relns" @if(isset($data['customer']['allowed_relns']) AND $data['customer']['allowed_relns'] != null) checked="checked" @endif>
+									<span class="custom-control-indicator"></span>
+									<span class="custom-control-label" style="font-size: 16px;">Allowed Relns</span>
+								</label>
 							</div>
 						</div>
 					</div>
