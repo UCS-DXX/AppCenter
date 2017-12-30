ee863df2be283e574905802422584eb8ce1c7472
Dipanjan Kundu <dipsraj.kundu@gmail.com>
2017-12-30 15:37:36 +0530

    Transaction Filters added

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -15,6 +15,9 @@ class TransactionController extends Controller
 
         $acc_no = $request->input('acc_no');
         $status = $request->input('status');
+        $transfer_date = $request->input('transfer_date');
+        $req_no = $request->input('req_no');
+        $bank_ref = $request->input('bank_ref');
 
         $transactions = $transactionModel
             ->where(function ($query)  use ($acc_no) {
@@ -22,9 +25,24 @@ class TransactionController extends Controller
                     $query->where('bene_account_no', 'like', '%' . $acc_no . '%');
                 }
             })
+            ->where(function ($query)  use ($transfer_date) {
+                if (sizeof($transfer_date)>0) {
+                    $query->where('transfer_date', 'like', '%' . $transfer_date . '%');
+                }
+            })
             ->where(function ($query)  use ($status) {
                 if (sizeof($status)>0) {
                     $query->where('status_code', 'like', '%' . $status . '%');
+                }
+            })
+            ->where(function ($query)  use ($req_no) {
+                if (sizeof($req_no)>0) {
+                    $query->where('req_no', 'like', '%' . $req_no . '%');
+                }
+            })
+            ->where(function ($query)  use ($bank_ref) {
+                if (sizeof($bank_ref)>0) {
+                    $query->where('bank_ref', 'like', '%' . $bank_ref . '%');
                 }
             })
             ->orderBy('req_timestamp', 'asc')->paginate(10);
--- resources/views/apps/transfer_money/transactions.blade.php
+++ resources/views/apps/transfer_money/transactions.blade.php
@@ -28,21 +28,42 @@
 					<div class="card-body">
 						<form data-toggle="md-validator" method="get" action="{{ url('/transactions') }}">
 							<div class="row">
-								<div class="col-sm-4">
+								<div class="col-sm-2">
 									<div class="md-form-group md-label-floating">
 										<input class="md-form-control" type="text" name="acc_no" spellcheck="false"
 											   data-msg-required="Please enter Beneficiary account no." value="{{ app('request')->input('acc_no') }}">
 										<label class="md-control-label">BENEFICIARY ACCOUNT NO</label>
 									</div>
 								</div>
-								<div class="col-sm-4">
+								<div class="col-sm-2">
+									<div class="md-form-group md-label-floating">
+										<input class="md-form-control" type="text" name="transfer_date" spellcheck="false"
+											   data-msg-required="Please enter Transfer Date" value="{{ app('request')->input('transfer_date') }}">
+										<label class="md-control-label">TRANSFER DATE</label>
+									</div>
+								</div>
+								<div class="col-sm-2">
+									<div class="md-form-group md-label-floating">
+										<input class="md-form-control" type="text" name="req_no" spellcheck="false"
+											   data-msg-required="Please enter Request no." value="{{ app('request')->input('req_no') }}">
+										<label class="md-control-label">REQUEST NO</label>
+									</div>
+								</div>
+								<div class="col-sm-2">
+									<div class="md-form-group md-label-floating">
+										<input class="md-form-control" type="text" name="bank_ref" spellcheck="false"
+											   data-msg-required="Please enter Bank Reference" value="{{ app('request')->input('bank_ref') }}">
+										<label class="md-control-label">BANK REFERENCE</label>
+									</div>
+								</div>
+								<div class="col-sm-2">
 									<div class="md-form-group md-label-floating">
 										<input class="md-form-control" type="text" name="status" spellcheck="false"
 											   data-msg-required="Please enter status" value="{{ app('request')->input('status') }}">
 										<label class="md-control-label">STATUS</label>
 									</div>
 								</div>
-								<div class="col-sm-4">
+								<div class="col-sm-2">
 									<div class="md-form-group md-label-floating" style="display:flex; flex-direction: row; justify-content: space-around">
 										<button class="btn btn-primary" type="submit">Filter</button>
 										<a class="btn btn-primary" href="{{ url('/transactions') }}">Reset</a>
@@ -55,57 +76,15 @@
 								<thead>
 									<tr>
 										<th>SHOW LINK</th>
-										{{--<th>ID</th>
-										<th>REQ NO</th>
-										<th>REQ VERSION</th>
-										<th>REQ TIMESTAMP</th>
-										<th>CUSTOMER ID</th>
-										<th>DEBIT ACCOUNT NO</th>
-										<th>BENE CODE</th>--}}
 										<th>BENEFICIARY FULL NAME</th>
-										{{--<th>BENE ADDRESS1</th>
-										<th>BENE ADDRESS2</th>
-										<th>BENE ADDRESS3</th>
-										<th>BENE POSTAL CODE</th>
-										<th>BENE CITY</th>
-										<th>BENE STATE</th>
-										<th>BENE COUNTRY</th>
-										<th>BENE CONTACT EMAIL ID</th>
-										<th>BENE CONTACT MOBILE NO</th>--}}
 										<th>BENEFICIARY ACCOUNT NO</th>
 										<th>BENEFICIARY ACCOUNT IFSC</th>
-										{{--<th>BENE ACCOUNT MOBILE NO</th>
-										<th>BENE ACCOUNT MMID</th>
-										<th>TRANSFER TYPE</th>--}}
 										<th>REMITTER NAME</th>
 										<th>TRANSFER DATE</th>
 										<th>TRANSFER AMOUNT</th>
-										{{--<th>TRANSFER CCY</th>
-										<th>RMTR TO BENE NOTE</th>--}}
 										<th>STATUS</th>
-										{{--<th>BANK REF</th>
-										<th>REP NO</th>
-										<th>REP VERSION</th>
-										<th>REP TIMESTAMP</th>
-										<th>ATTEMPT NO</th>
-										<th>FAULT CODE</th>
-										<th>FAULT REASON</th>
-										<th>REQ TRANSFER TYPE</th>
-										<th>SERVICE ID</th>
-										<th>RECONCILED AT</th>
-										<th>CBS REQ REF NO</th>
-										<th>APP ID</th>
-										<th>SUB CODE</th>
-										<th>NAME WITH BENE BANK</th>
-										<th>NOTIFY STATUS</th>
-										<th>NOTIFY ATTEMPT NO</th>
-										<th>NOTIFY ATTEMPT AT</th>
-										<th>NOTIFIED AT</th>
-										<th>NOTIFY RESULT</th>
-										<th>AADHAAR NO</th>
-										<th>AADHAAR MOBILE NO</th>
-										<th>BENE BANK NAME</th>
-										<th>PURPOSE CODE</th>--}}
+										<th>REQUEST NO.</th>
+										<th>BANK REFERENCE</th>
 									</tr>
 								</thead>
 								<tbody>
@@ -114,57 +93,15 @@
 											<td>
 												<a href="#">Show</a>
 											</td>
-											{{--<td>{{ $value['id'] }}</td>
-											<td>{{ $value['req_no'] }}</td>
-											<td>{{ $value['req_version'] }}</td>
-											<td>{{ $value['req_timestamp'] }}</td>
-											<td>{{ $value['customer_id'] }}</td>
-											<td>{{ $value['debit_account_no'] }}</td>
-											<td>{{ $value['bene_code'] }}</td>--}}
 											<td>{{ $transaction->bene_full_name }}</td>
-											{{--<td>{{ $value['bene_address1'] }}</td>
-											<td>{{ $value['bene_address2'] }}</td>
-											<td>{{ $value['bene_address3'] }}</td>
-											<td>{{ $value['bene_postal_code'] }}</td>
-											<td>{{ $value['bene_city'] }}</td>
-											<td>{{ $value['bene_state'] }}</td>
-											<td>{{ $value['bene_country'] }}</td>
-											<td>{{ $value['bene_contact_email_id'] }}</td>
-											<td>{{ $value['bene_contact_mobile_no'] }}</td>--}}
 											<td>{{ $transaction->bene_account_no }}</td>
 											<td>{{ $transaction->bene_account_ifsc }}</td>
-											{{--<td>{{ $value['bene_account_mobile_no'] }}</td>
-											<td>{{ $value['bene_account_mmid'] }}</td>
-											<td>{{ $value['transfer_type'] }}</td>--}}
 											<td></td>
-											<td></td>
+											<td>{{ $transaction->transfer_date }}</td>
 											<td>{{ $transaction->transfer_amount }}</td>
-											{{--<td>{{ $value['transfer_ccy'] }}</td>
-											<td>{{ $value['rmtr_to_bene_note'] }}</td>--}}
 											<td>{{ $transaction->status_code }}</td>
-											{{--<td>{{ $value['bank_ref'] }}</td>
-											<td>{{ $value['rep_no'] }}</td>
-											<td>{{ $value['rep_version'] }}</td>
-											<td>{{ $value['rep_timestamp'] }}</td>
-											<td>{{ $value['attempt_no'] }}</td>
-											<td>{{ $value['fault_code'] }}</td>
-											<td>{{ $value['fault_reason'] }}</td>
-											<td>{{ $value['req_transfer_type'] }}</td>
-											<td>{{ $value['service_id'] }}</td>
-											<td>{{ $value['reconciled_at'] }}</td>
-											<td>{{ $value['cbs_req_ref_no'] }}</td>
-											<td>{{ $value['app_id'] }}</td>
-											<td>{{ $value['sub_code'] }}</td>
-											<td>{{ $value['name_with_bene_bank'] }}</td>
-											<td>{{ $value['notify_status'] }}</td>
-											<td>{{ $value['notify_attempt_no'] }}</td>
-											<td>{{ $value['notify_attempt_at'] }}</td>
-											<td>{{ $value['notified_at'] }}</td>
-											<td>{{ $value['notify_result'] }}</td>
-											<td>{{ $value['aadhaar_no'] }}</td>
-											<td>{{ $value['aadhaar_mobile_no'] }}</td>
-											<td>{{ $value['bene_bank_name'] }}</td>
-											<td>{{ $value['purpose_code'] }}</td>--}}
+											<td>{{ $transaction->req_no }}</td>
+											<td>{{ $transaction->bank_ref }}</td>
 										</tr>
 									@endforeach
 								</tbody>
